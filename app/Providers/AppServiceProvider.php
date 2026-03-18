6d016c73dc568efbe1210cb8e3e90f08dcebe9ae
SebUbu <rogaseb@gmail.com>
2026-03-18 13:27:36 +0100

    Add SQL query logging and configure logging channel for SQL queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::listen(function ($query) {
+            Log::channel('sql')->info('SQL QUERY', [
+                'timestamp' => now()->toDateTimeString(),
+                'url' => Request::fullUrl(),
+                'method' => Request::method(),
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+                'time_ms' => $query->time,
+                'slow_query' => $query->time > 10,
+            ]);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    DB::select('select 1');
-
     return view('welcome');
 });
